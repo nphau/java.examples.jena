e9fa374314172f425b2049386c121369485f9962
nphau <tieuhauvn@gmail.com>
2022-06-03 10:30:04 +0700

    feat: add index.html

---
+++ heroku_deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+heroku war:deploy target/owl-1.0.1.war --app carowl
--- src/main/java/sg/nphau/java/owl/controllers/CarsController.java
+++ src/main/java/sg/nphau/java/owl/controllers/CarsController.java
@@ -5,9 +5,10 @@
  */
 package sg.nphau.java.owl.controllers;
 
-import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,22 +22,13 @@ import java.util.List;
 import java.util.concurrent.Future;
 
 @RestController
+@EnableAutoConfiguration
+@Component
 @EnableAsync
 public class CarsController {
 
-    private final Gson gson;
-    private final CarsRepository carsRepository;
-
     @Autowired
-    public CarsController(CarsRepository carsRepository, Gson gson) {
-        this.carsRepository = carsRepository;
-        this.gson = gson;
-    }
-
-    @GetMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
+    private CarsRepository carsRepository;
 
     @GetMapping("/cars")
     @ResponseBody
